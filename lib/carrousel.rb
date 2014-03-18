7c7d5d8267fae61e023440846431a9620bc5b0a9
Raj Sahae <rsahae@teslamotors.com>
2014-03-18 14:41:49 -0700

    Moved Carrousel Runner file to more standard file structure.

--- lib/carrousel.rb
+++ lib/carrousel.rb
@@ -2,8 +2,7 @@
 # enconding: UTF-8
 
 require "carrousel/version"
-require 'digest'
-require 'yaml'
+require 'carrousel/runner'
 
 ##
 # "Enter the Carrousel. This is the time of renewal."
@@ -11,91 +10,4 @@ require 'yaml'
 # Carrousel is a system command execution and tracker utility.
 module Carrousel
 
-  class Runner
-
-    def initialize(args, opts = {})
-      @args = args
-      @opts = opts
-      @incomplete = []
-      @complete = []
-
-      unless @opts[:listfile].nil?
-        lines = File.readlines(@opts[:listfile]).map(&:strip)
-        @incomplete.concat(lines)
-      end
-      @incomplete.concat(@args)
-
-      @opts[:statusfile] ||= generate_status_filename
-      open_status_file
-
-      p self if @opts[:debug]
-
-      raise ArgumentError.new("Command option is required") if @opts[:command].nil?
-    end # def initialize
-
-    def run
-      # Loop over everything in the list. Run the command. If the command fails
-      # then we move the item to the bottom of the list. If the command
-      # succeeds, we move the item to the completed list. If we are interrupted
-      # in the middle of processing, we ensure that the item is saved in the
-      # normal list, and we ensure that we write out the completed list.
-      until @incomplete.empty?
-        begin
-          command = [@opts[:command], @incomplete.first].join(' ')
-          warn "Executing command: #{command}" if @opts[:verbose]
-          resp = system(command)
-          warn "System response: #{resp}" if @opts[:verbose]
-          if resp
-            @complete << @incomplete.delete(@incomplete.first)
-          else
-            @incomplete.rotate!
-          end
-        ensure
-          save_status_file
-        end
-
-        if @opts[:delay] > 0
-          warn "Sleeping for #{@opts[:delay]} seconds" if @opts[:verbose]
-          sleep @opts[:delay]
-        end
-      end # until @incomplete.empty?
-    end # def run
-
-    private
-    def generate_status_filename
-      key = Digest::SHA256.hexdigest(@incomplete.sort.join).slice(0...7)
-      warn "status file key: #{key}" if @opts[:debug]
-      name = self.class.name.gsub('::', '_').downcase
-      File.expand_path(".#{name}_status_#{key}", Dir.pwd)
-    end # def generate_status_filename
-
-    private
-    def open_status_file
-      if File.exists?(@opts[:statusfile])
-        dbs = YAML.load(File.read(@opts[:statusfile]))
-        warn "opened status file:\n#{dbs}" if @opts[:debug]
-        if dbs
-          @opts[:command] ||= dbs[:command]
-          @complete.concat(dbs[:complete])
-          @incomplete.concat(dbs[:incomplete])
-        end
-      end
-    end # def open_status_file
-
-    private
-    def save_status_file
-      warn "Saving status file: #{@opts[:statusfile]}" if @opts[:verbose]
-      File.open(@opts[:statusfile], 'w') do |f|
-        ydb = {
-          :command => @opts[:command],
-          :complete => @complete,
-          :incomplete => @incomplete
-        }.to_yaml
-        f.puts(ydb)
-        warn "Saved status file:\n#{ydb}" if @opts[:debug]
-      end
-      true
-    end # def save_status_file
-
-  end # class Runner
 end # module Carrousel
